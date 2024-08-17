1e70b3af61a85c6a2376964e39761e8e90f16022
fedotovps <fedotovps@bk.ru>
2024-08-17 20:30:32 +0300

    Инициализация нового проекта сервера

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const multer = require("multer");
+
+const uploadDestination = "uploads";
+
+// Показываем где хранить файлы
+const storage = multer.diskStorage({
+  destination: uploadDestination,
+  filename: function (req, file, next) {
+    next(null, file.originalname);
+  },
+});
+
+const uploads = multer({ storage: storage });
+
+router.get("/register", (req, res) => {
+  res.send("register");
+});
+
+module.exports = router;
