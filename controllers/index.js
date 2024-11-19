37e2ab2cb640f462ca2928df0762979dd7a27cad
fedotovps <fedotovps@bk.ru>
2024-11-19 18:37:13 +0300

    fix импорта контроллера велком-скринов

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,7 @@ const OrderController = require("./order");
 const StudentController = require("./student");
 const TutorController = require("./tutor");
 const EmployeeController = require("./employee");
+const WelcomeScreenController = require("./welcomScreen");
 
 module.exports = {
   UserController,
@@ -10,4 +11,5 @@ module.exports = {
   TutorController,
   EmployeeController,
   OrderController,
+  WelcomeScreenController,
 };
