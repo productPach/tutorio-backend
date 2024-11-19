fbfa0c086a9d3c19d9b97c6948be8db2b5322302
fedotovps <fedotovps@bk.ru>
2024-11-19 18:55:27 +0300

    Добавление контроллера создания сотрудника

--- controllers/employee.js
+++ controllers/employee.js
@@ -3,7 +3,40 @@ const { prisma } = require("../prisma/prisma-client");
 const EmployeeController = {
   // Создание сотрудника
   createEmployee: async (req, res) => {
-    res.send("createEmployee");
+    const { name, email, region, status } = req.body;
+    const userId = req.user.userID;
+
+    if (!name || !email || !status) {
+      return res
+        .status(400)
+        .json({ error: "Не заполнены все обязательные поля" });
+    }
+
+    try {
+      const existingEmployee = await prisma.employee.findFirst({
+        where: { email: email },
+      });
+
+      if (existingEmployee) {
+        return res.status(400).json({
+          error: "Сотрудник уже существует",
+        });
+      }
+
+      const employee = await prisma.employee.create({
+        data: {
+          userId,
+          name,
+          email,
+          region: region || undefined,
+          status,
+        },
+      });
+      res.json(tutor);
+    } catch (error) {
+      console.log("Error Create Employee", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
   },
   // Получение всех сотрудников
   getAllEmployees: async (req, res) => {
