8bebad13fd3533d807f99ceaf602028bdb434866
fedotovps <fedotovps@bk.ru>
2025-03-06 19:16:45 +0300

    фикс контроллера рассылок

--- controllers/mail.js
+++ controllers/mail.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const MAILOPOST_API_URL = "https://api.mailopost.ru/v1/messages/send";
+const MAILOPOST_API_URL = "https://api.mailopost.ru/v1";
 const API_TOKEN = process.env.MAILOPOST_API_TOKEN;
 
 const MailController = {
@@ -14,9 +14,10 @@ const MailController = {
 
     try {
       const response = await axios.post(
-        MAILOPOST_API_URL,
+        `${MAILOPOST_API_URL}/email/messages`,
         {
-          from: "your-email@example.com", // Замените на ваш email
+          from_email: "myrepetitorinfo@yandex.ru", // Укажите ваш email-отправитель
+          from_name: "Павел", // Укажите имя отправителя
           to,
           subject,
           text,
@@ -24,16 +25,24 @@ const MailController = {
         {
           headers: {
             Authorization: `Bearer ${API_TOKEN}`,
+            "Content-Type": "application/json",
           },
         }
       );
 
-      res
-        .status(200)
-        .json({ message: "Письмо отправлено", data: response.data });
+      res.status(200).json({
+        message: "Письмо отправлено",
+        data: response.data,
+      });
     } catch (error) {
-      console.error("Ошибка при отправке письма:", error);
-      res.status(500).json({ error: "Ошибка при отправке письма" });
+      console.error(
+        "Ошибка при отправке письма:",
+        error.response?.data || error.message
+      );
+      res.status(error.response?.status || 500).json({
+        error: "Ошибка при отправке письма",
+        details: error.response?.data || error.message,
+      });
     }
   },
 };
