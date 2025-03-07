d33124b87b19e33eb97154dee501806432f48e62
fedotovps <fedotovps@bk.ru>
2025-03-08 00:07:50 +0300

    fix

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3001",
+    origin: ["http://localhost:3001", "http://158.160.78.58:3001"], // Добавь внешний IP
+    credentials: true, // если нужно передавать куки или авторизационные заголовки
   },
 });
 
@@ -21,7 +22,13 @@ const io = new Server(server, {
 require("./sockets/emailVerificationSocket")(io);
 
 // CORS middleware setup
-app.use(cors({ origin: "http://localhost:3001" })); // Настраиваем разрешение запросов с определенного источника
+//app.use(cors({ origin: "http://localhost:3001" })); // Настраиваем разрешение запросов с определенного источника
+app.use(
+  cors({
+    origin: ["http://localhost:3001", "http://158.160.78.58:3001"],
+    credentials: true,
+  })
+);
 
 app.use(logger("dev"));
 app.use(express.json());
