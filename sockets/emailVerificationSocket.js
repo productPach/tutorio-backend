04755d9321f5b5d8695496243095e097ed7b3a43
fedotovps <fedotovps@bk.ru>
2025-03-10 15:19:27 +0300

    fix socket

--- sockets/emailVerificationSocket.js
+++ sockets/emailVerificationSocket.js
@@ -1,13 +1,23 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (io) => {
+  // Мапа для отслеживания текущих пользователей, уже подключенных к комнате
+  const connectedTutors = new Map();
+
   io.on("connection", (socket) => {
     console.log("Пользователь подключился:", socket.id);
 
-    // Сохраняем связь tutorId с socket.id
+    // Сохраняем связь tutorId с socket.id, но только если пользователь ещё не подключен
     socket.on("registerTutor", (tutorId) => {
-      socket.join(tutorId); // Сохраняем сокет в комнате, соответствующей tutorId
-      console.log(`Tutor ${tutorId} подключился к комнате ${socket.id}`);
+      if (!connectedTutors.has(tutorId)) {
+        // Если репетитор ещё не подключен, подключаем к комнате
+        socket.join(tutorId);
+        connectedTutors.set(tutorId, socket.id); // Добавляем tutorId в мапу
+        console.log(`Tutor ${tutorId} подключился к комнате ${tutorId}`);
+      } else {
+        // Если уже подключен, просто сообщаем
+        console.log(`Tutor ${tutorId} уже в комнате`);
+      }
     });
 
     socket.on("verifyEmail", (token) => {
@@ -29,6 +39,14 @@ module.exports = (io) => {
     });
 
     socket.on("disconnect", () => {
+      // Удаляем tutorId из мапы при отключении
+      for (let [tutorId, socketId] of connectedTutors) {
+        if (socketId === socket.id) {
+          connectedTutors.delete(tutorId);
+          console.log(`Tutor ${tutorId} отключился от комнаты`);
+          break;
+        }
+      }
       console.log("Пользователь отключился:", socket.id);
     });
   });
