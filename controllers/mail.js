f495ae1fc75d52246a7f95349beae8fc8c9a84cc
fedotovps <fedotovps@bk.ru>
2025-03-16 11:36:41 +0300

    фикс шаблона письма для подтверждения почты

--- controllers/mail.js
+++ controllers/mail.js
@@ -93,50 +93,53 @@ const MailController = {
       );
 
       // HTML-шаблон письма
-      const html = `<!DOCTYPE html><html lang="ru"><head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Подтверждение почты tutorio</title>
-            <link href="https://fonts.googleapis.com/css2?family=Fira+Sans&amp;family=Montserrat:wght@400;500;600&amp;family=PT+Sans&amp;family=Poppins:wght@400;600&amp;family=Sofia+Sans:wght@900&amp;family=Source+Sans+3:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
-        </head>
-        <body>
-            <div style="background-color:#F1F2F4;display:flex;flex-direction:column;align-items:center;">
-                <div style="display:flex;background-color:white;width:100%;max-width:600px;flex-direction:column;align-items:center;justify-content:center;height:100vh;">
-                    <div style="max-width: 400px;">
-                <div style="display:flex;flex-direction:column;gap:0px;margin-bottom:150px;">
-                    <div style="font-family:Sofia Sans,sans-serif;font-weight:900;font-size:32px;">Tutorio</div>
-                    <span style="font-family:Fira Sans,sans-serif;font-weight: 400;font-size: 9px;">Онлайн-сервис подбора репетиторов</span>
-                </div>
-                <div style="width: 200px;">
-                    
-                </div>
-                <div>
-                    <h1 style="font-family:Verdana,Geneva,sans-serif;margin:10px 0 0 0;font-size:24px;">
-                        Подтверждение почты
-                       </h1>
-                       <p style="font-family:Verdana,Geneva,sans-serif;margin:5px 0 20px 0;font-size:14px;">
-                        Просто нажмите на&nbsp;эту кнопку и&nbsp;готово
-                       </p>
-                       <div style="margin-bottom:20px;">
-                        <a href="${domain}/verify-email?token=${emailVerificationToken}" style="font-family:Verdana,Geneva,sans-serif;background-color:#333333;color:#ffffff;display:inline-block;font-family:Verdana,Geneva,sans-serif;font-size:14px;font-weight:bold;line-height:56px;text-align:center;text-decoration:none;width:100%;-webkit-text-size-adjust:none;border-radius:10px;" target="_blank" class="MsoNormal_mr_css_attr" rel=" noopener noreferrer">
-                            Подтвердить
-                        </a>
-                       </div>
-                        <p style="font-family:Verdana,Geneva,sans-serif;margin:0 0 10px 0;font-size:13px;">
-                            Или пройдите по&nbsp;ссылке:
-                        </p>
-                        <a href="${domain}/verify-email?token=${emailVerificationToken}" style="font-family:Verdana,Geneva,sans-serif;font-size:14px;color:#222222;text-decoration:underline;" target="_blank" rel=" noopener noreferrer">
-                            https://tutorio.ru/verify-email
-                        </a>
-                        <p style="font-family:Verdana,Geneva,sans-serif;margin:10px 0;font-size:12px;color:#888888;">
-                            Если вы&nbsp;не&nbsp;оставляли эту почту в&nbsp;Tutorio&nbsp;—
-                            не&nbsp;нажимайте. Кто-то ошибся почтой и&nbsp;указал вашу
-                        </p>
-                </div>
-              </div>
-            </div>
-            </div>
-        </body></html>`;
+      const html = `<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Подтверждение почты tutorio</title>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans&family=Montserrat:wght@400;500;600&family=PT+Sans&family=Poppins:wght@400;600&family=Sofia+Sans:wght@900&family=Source+Sans+3:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body style="margin: 0;font-family:'Source Sans 3',sans-serif"">
+    <div style="background-color:#F1F2F4;padding: 60px 0;display:flex;flex-direction:column;align-items:center;">
+        <div style="display:flex;;width:100%;max-width:600px;flex-direction:column;align-items:center;justify-content:center;background-color: white;border-radius: 10px;padding: 60px 0">
+            <div style="max-width: 400px;">
+        <div style="display:flex;flex-direction:column;gap:0px;margin-bottom:50px;">
+            <div style="font-family:Sofia Sans,sans-serif;font-weight:900;font-size:32px;">Tutorio</div>
+            <span style="font-family:Fira Sans,sans-serif;font-weight: 400;font-size: 9px;">Онлайн-сервис подбора репетиторов</span>
+        </div>
+        <div style="width:200px;">
+            <img style="width:200px;" src="/media//img_email/mail.svg">
+        </div>
+        <div>
+            <h1 style="font-family:'Source Sans 3',sans-serif;margin:10px 0 0 0;font-size:28px;">
+                Подтверждение почты
+               </h1>
+               <p style="font-family:'Source Sans 3',sans-serif;margin:5px 0 20px 0;font-size:16px;">
+                Просто нажмите на&nbsp;эту кнопку и&nbsp;готово
+               </p>
+               <div style="margin-bottom:20px;">
+                <a href="${domain}/verify-email?token=${emailVerificationToken}" style="font-family:'Source Sans 3',sans-serif;background-color:#fad949;color:#333333;display:inline-block;font-size:18px;font-weight:700;line-height:56px;text-align:center;text-decoration:none;width:100%;-webkit-text-size-adjust:none;border-radius:10px;" target="_blank" class="MsoNormal_mr_css_attr" rel=" noopener noreferrer">
+                    Подтвердить
+                </a>
+               </div>
+                <p style="font-family:'Source Sans 3',sans-serif;margin:0 0 10px 0;font-size:16px;">
+                    Или пройдите по&nbsp;ссылке:
+                </p>
+                <a href="${domain}/verify-email?token=${emailVerificationToken}" style="font-family:'Source Sans 3',sans-serif;font-size:16px;color:#222222;text-decoration:underline;" target="_blank" rel=" noopener noreferrer">
+                    https://tutorio.ru/verify-email
+                </a>
+                <p style="font-family:'Source Sans 3',sans-serif;margin:10px 0;font-size:14px;color:#888888;">
+                    Если вы&nbsp;не&nbsp;оставляли эту почту в&nbsp;Tutorio&nbsp;—
+                    не&nbsp;нажимайте. Кто-то ошибся почтой и&nbsp;указал вашу
+                </p>
+        </div>
+      </div>
+    </div>
+    </div>
+</body>
+</html>`;
 
       // Отправка письма
       const response = await axios.post(
