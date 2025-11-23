f284449aae4bbdc15ef7591f7a556519e5299af5
fedotovps <fedotovps@bk.ru>
2025-11-23 17:13:53 +0300

    фикс prisma

--- prisma/prisma-client.js
+++ prisma/prisma-client.js
@@ -1,10 +1,6 @@
 const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-const prisma = new PrismaClient({
-  adapter: {
-    provider: "mongodb",
-    url: process.env.DATABASE_URL,
-  },
-});
-
-module.exports = { prisma };
+module.exports = {
+  prisma,
+};
